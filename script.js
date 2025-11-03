a4039a5db4a5a951026ce967b4f50dd8060cc4ef
PedroStabach <pedroaugustostabach3@gmail.com>
2025-11-02 23:03:48 -0300

    utilizando arrow function + consoles

--- script.js
+++ script.js
@@ -18,14 +18,26 @@ function divisao(a, b) {
     return a / b;
 }
 
+function aplicarFuncao(num, func) {
+    return func(num);
+}
+
 //CONSTANTES
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const filtrarPares = function(array) {
+const dobro = n => n * 2;
+
+const filtrarPares = (array) => {
     return array.filter(num => num % 2 === 0); // arrow function dentro
 };
 
-const calcularMedia = function(array) {
+const calcularMedia = (array) => {
     const soma = array.reduce((acc, num) => acc + num, 0); // arrow function dentro
     return soma / array.length;
 };
+
+//CONSOLES
+console.log("Números pares: ", filtrarPares(numeros));
+console.log("Média dos números: ", calcularMedia(numeros));
+console.log("Dobro de 5: ", dobro(5));
+console.log("Aplicando callback (dobro) ao número 10: ", aplicarFuncao(10, dobro));
